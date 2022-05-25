9ac843afaa2c604345b7b8bb7c2426b41d03e6b6
matheus lima <matheusmt31@gmail.com>
2022-05-24 22:40:10 -0300

    Initial commit + adding dependencies

---
+++ app/src/main/java/com/br/matheus/correa/moviedb/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.br.matheus.correa.moviedb.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.matheus.correa.moviedb.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
